33d49f8d4349a4dc43d213a5132380c2a2deee92
polina193535 <kostikova658@gmail.com>
2025-10-30 23:03:40 +0300

    test: add test for deleting customer

---
+++ tests/test_delete_customer.py
@@ -0,0 +1,26 @@
+from pages.customer_page import CustomersPage
+
+from waits import Waits
+
+URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager"
+
+
+def test_delete_customer(browser):
+    page = CustomersPage(browser)
+    page.open(URL)
+    page.go_to_customers_tab()
+    first_names = page.get_first_names()
+    assert first_names, "Нет имен"
+
+    lengths = [len(name) for name in first_names]
+    average = sum(lengths) / len(lengths)
+    closest_name = min(first_names, key=lambda name: abs(len(name) - average))
+    page.delete_customer(closest_name)
+
+    Waits.for_element(
+        browser,
+        (page.ROWS),
+        timeout=5,
+    )
+    updated_names = page.get_first_names()
+    assert closest_name not in updated_names, f"Клиент {closest_name} не был удалён!"
