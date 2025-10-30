d96421d88c516e6957e5c0a78dfee3d055315e7b
polina193535 <kostikova658@gmail.com>
2025-10-30 23:02:16 +0300

    test: add test for adding a customer

---
+++ tests/test_add_customer.py
@@ -0,0 +1,33 @@
+import random
+import string
+
+from pages.add_customer_page import AddCustomerPage
+from pages.customer_page import CustomersPage
+
+URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager"
+
+
+def generate_post_code() -> str:
+    return "".join(random.choices(string.digits, k=10))
+
+
+def post_code_to_first_name(post_code: str) -> str:
+    first_name = ""
+    for i in range(0, 10, 2):
+        pair = int(post_code[i : i + 2])
+        letter = chr(ord("a") + (pair % 26))
+        first_name += letter
+    return first_name
+
+
+def test_add_customer(browser):
+    post_code = generate_post_code()
+    first_name = post_code_to_first_name(post_code)
+    add_page = AddCustomerPage(browser)
+    add_page.open(URL)
+    add_page.add_customer(first_name, first_name, post_code)
+
+    customers_page = CustomersPage(browser)
+    customers_page.go_to_customers_tab()
+    first_names = customers_page.get_first_names()
+    assert first_name in first_names, f"Клиент {first_name} не найден в таблице!"
