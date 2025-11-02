7b27baf38620c90e9803c533af544f55b8a99da9
polina193535 <kostikova658@gmail.com>
2025-11-02 17:36:03 +0300

    refactor: move urls and paths to config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+BASE_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject"
+"""
+Базовый URL для приложения.
+"""
+
+MANAGER_PATH = "#/manager"
+"""
+Путь к странице менеджера.
+"""
+
+CUSTOMER_PATH = "#/customer"
+"""
+Путь к странице клиента.
+"""
+
+def url(path: str) -> str:
+    """
+    Формирует полный URL на основе базового адреса и относительного пути.
+    """
+    return f"{BASE_URL}/{path}"
